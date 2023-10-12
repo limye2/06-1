5877aebe2662a89b5c113cf9b35f077485a0271f
limye2 <144992260+limye2@users.noreply.github.com>
2023-10-12 15:41:40 +0900

    Initial Commit

---
+++ 06-1/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  06-1
+//
+//  Created by MacBook Air on 2023/10/12.
+//
+
+#include <stdio.h>
+
+void print_star(void)
+{
+    int i;
+    for(i=0; i<10; i++)
+        printf("*");
+}
+
+int main(void)
+{
+    
+    print_star();
+    print_star();
+    print_star();
+    
+    return 0;
+}
